24b675781cd1dc39c506d8c71fc85b628021ac76
Mouaad Aallam <mouaad@aallam.com>
2023-02-04 16:22:15 +0100

    chore: use ulid for insights tests

--- test/insights_client_test.dart
+++ test/insights_client_test.dart
@@ -1,5 +1,6 @@
 import 'package:algolia_lite/algolia_lite.dart';
 import 'package:test/test.dart';
+import 'package:ulid4d/ulid4d.dart';
 
 void main() {
   test('Send events', () async {
@@ -7,14 +8,14 @@ void main() {
       applicationID: 'latency',
       apiKey: 'afc3dd66dd1293e2e2736a5a51b05c0a',
     );
-
+    final userToken = ULID.randomULID();
     await client.sendEvents(
       [
-        const InsightEvent(
+        InsightEvent(
           eventType: EventType.click,
           eventName: 'Clicked Search Result',
           index: 'instant_search',
-          userToken: 'anonymous-xxxxxx-xx-xxx-xxxxxx',
+          userToken: userToken,
           queryID: '43b15df305339e827f0ac0bdc5ebcaa7',
           objectIDs: ['item'],
           positions: [1],
